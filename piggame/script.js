b3040740370f902cd0c06eb5654b275ecbddf5bd
Jerry Yasir <yasir.attiq@gmail.com>
2025-09-01 23:26:17 -0400

    New changes

---
+++ piggame/script.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log("Welcome to the Pig Game!");
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+
+
+
+
+
+
