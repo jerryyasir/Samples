6b318a2f5618e9d90a4e1d2618dcc42d2300f50a
Jerry Yasir <yasir.attiq@gmail.com>
2025-09-25 22:45:45 -0400

    commit 9f8c3e2b5d6a7e8c9b0f1a2b3c4d5e6f7g8h9i0j

--- Update-GroupPermissionsOnSite.ps1
+++ Update-GroupPermissionsOnSite.ps1
@@ -1,4 +1,4 @@
-$AllreportData = @()
+$global:AllReportData = @()
 
 [string]$SiteUrl = "https://30jk56.sharepoint.com/sites/JustBasicTeamSite" 
 $TargetAdminGroupName = "*admins" 
@@ -34,71 +34,13 @@ $excludedLists = @(
 # Connect to the SharePoint site
 Connect-PnPOnline -Url $siteUrl -ClientId "c7bf16b8-a2cb-4a5b-8080-b00016bb59ae" -Tenant "77c0f0fc-3049-4c54-a5d5-ff32be8ffd59" -Thumbprint "961A05530B35BF73165BBCA0A90F83D82CF25C24" -Verbose
 
-#$web = Get-PnPWeb
-#$subWebs = Get-PnPSubWeb -Recurse
-
-# $lists = Get-PnPList | ? {$_.Hidden -eq $false}
-
-# foreach ($list in $lists) {
-#     $SecurableObject = $list 
-#     $ObjectName = $SecurableObject.Title
-#     $hasUniquePermissions = Get-PnPProperty -ClientObject $SecurableObject -Property HasUniqueRoleAssignments
-#     $ObjectType = "List"
-#     if ($hasUniquePermissions) {
-#         Write-Host "Checking $ObjectType : $ObjectName"
-        
-#         # Get role assignments
-#         $roleAssignments = Get-PnPProperty -ClientObject $SecurableObject -Property RoleAssignments
-        
-#         foreach ($roleAssignment in $roleAssignments) {
-#             $principal = Get-PnPProperty -ClientObject $roleAssignment -Property Member
-#             $roleBindings = Get-PnPProperty -ClientObject $roleAssignment -Property RoleDefinitionBindings
-#             $principal.Title.ToLower()
-#             # Check if the principal is the target group
-#             if ($principal.Title.ToLower() -like $TargetAdminGroupName -or $principal.Title.ToLower() -like $TargetOwnerGroupName) {
-#                 foreach ($roleBinding in $roleBindings) {
-#                     if ($roleBinding.Name -eq $CurrentAdminPermissionLevel -or $roleBinding.Name -eq $CurrentOwnerPermissionLevel) {
-#                         if ($ReportingOnly) {
-#                             # Add to report data
-#                             $reportData += [PSCustomObject]@{
-#                                 ObjectType = $ObjectType
-#                                 ObjectName = $ObjectName
-#                                 GroupName  = $principal.Title
-#                                 PermissionLevel = $roleBinding.Name
-#                                 SiteUrl    = $SiteUrl
-#                             }
-#                             Write-Host "Found $CurrentPermissionLevel for $principal.Title in $ObjectType : $ObjectName (Reporting Only)"
-#                         } else {
-#                             Write-Host "Found $CurrentPermissionLevel for $principal.Title in $ObjectType : $ObjectName. Replacing with $NewPermissionLevel."
-                            
-#                             # Remove the current permission
-#                             $SecurableObject.RoleAssignments.GetByPrincipal($principal).RoleDefinitionBindings.Remove($roleBinding)
-#                             $SecurableObject.Update()
-#                             Invoke-PnPQuery
-                            
-#                             # Add the new permission
-#                             Set-PnPListItemPermission -List $ObjectName -Identity $SecurableObject -User $principal.Title -AddRole $NewPermissionLevel -ErrorAction SilentlyContinue
-#                             Write-Host "Permission updated to $NewPermissionLevel for $principal.Title in $ObjectType : $ObjectName."
-#                         }
-#                     }
-#                 }
-#             }
-#         }
-#     }
-# }
-
-#$reportData | Out-GridView
-
-# Array to store report data
-
-
 # Function to check and update or report permissions
 function Process-Permissions {
     param (
         [Parameter(Mandatory=$true)] $SecurableObject,
         [Parameter(Mandatory=$true)] $ObjectName,
         [Parameter(Mandatory=$true)] $ObjectType,
-        [Parameter(Mandatory=$false)] $ReportingOnly
+        [Parameter(Mandatory=$false)]$ReportingOnly
     )
     $reportData = @()
     # Check if the object has unique permissions
@@ -108,12 +50,25 @@ function Process-Permissions {
         $hasUniquePermissions = Get-PnPProperty -ClientObject $SecurableObject -Property HasUniqueRoleAssignments
     }
 
+    $WebUrl = ""
+     if($ObjectType -eq "Web" -or $ObjectType -eq $ObjectType -eq "SubSite") {
+        $WebUrl = $SecurableObject.Url
+    }
+    
+    $ListTitle = ""
+    if($ObjectType -eq "List/Library") {
+        $ListTitle = $SecurableObject.Title
+    }
+
     if ($hasUniquePermissions) {
         Write-Host "Checking $ObjectType : $ObjectName"
         
+        $FolderId = ""
         if($ObjectType -eq "Folder") {
             Write-Host "Checking folder permissions"
             $roleAssignments = $SecurableObject.ListItemAllFields.RoleAssignments
+            $FolderId = $SecurableObject.ListItemAllFields.Id
+            $FolderId
         } else {
             $roleAssignments = Get-PnPProperty -ClientObject $SecurableObject -Property RoleAssignments
         }
@@ -121,24 +76,26 @@ function Process-Permissions {
         foreach ($roleAssignment in $roleAssignments) {
             $principal = Get-PnPProperty -ClientObject $roleAssignment -Property Member
             $roleBindings = Get-PnPProperty -ClientObject $roleAssignment -Property RoleDefinitionBindings
-            $principal.Title
-            $roleBindings
+            #$principal.Title
+            #$roleBindings
             # Check if the principal is the target group
             if ($principal.Title.ToLower() -like $TargetAdminGroupName -or $principal.Title.ToLower() -like $TargetOwnerGroupName) {
                 foreach ($roleBinding in $roleBindings) {
                      if ($roleBinding.Name -eq $CurrentAdminPermissionLevel -or $roleBinding.Name -eq $CurrentOwnerPermissionLevel) {
                         if ($ReportingOnly) {
                             # Add to report data
-                            $reportData += [PSCustomObject]@{
+                            $global:AllReportData+= [PSCustomObject]@{
                                 ObjectType = $ObjectType
                                 ObjectName = $ObjectName
+                                ObjectId = $SecurableObject.Id
                                 GroupName  = $principal.Title
                                 PermissionLevel = $roleBinding.Name
+                                WebUrl    = $WebUrl
                                 SiteUrl    = $SiteUrl
                             }
-                            Write-Host "Found $CurrentPermissionLevel for $principal.Title in $ObjectType : $ObjectName (Reporting Only)"
+                            Write-Host "Found $CurrentAdminPermissionLevel or $CurrentOwnerPermissionLevel  in $ObjectType : $ObjectName (Reporting Only)"
                         } else {
-                            Write-Host "Found $CurrentPermissionLevel for $principal.Title in $ObjectType : $ObjectName. Replacing with $NewPermissionLevel."                            
+                            Write-Host "Found $CurrentAdminPermissionLevel or or $CurrentOwnerPermissionLevel in $ObjectType : $ObjectName. Replacing with $NewPermissionLevel."                            
                             # Remove the current permission
                             $SecurableObject.RoleAssignments.GetByPrincipal($principal).RoleDefinitionBindings.Remove($roleBinding)
                             $SecurableObject.Update()
@@ -158,7 +115,6 @@ function Process-Permissions {
             Write-Host "$($ObjectType) : $($ObjectName) does not have unique permissions. Skipping." -ForegroundColor Yellow
         }
     }
-    return $reportData
 }
 
 # Get all subsites
@@ -166,11 +122,11 @@ $web = Get-PnPWeb
 $subWebs = Get-PnPSubWeb -Recurse
 
 # Check permissions for the root web
-$AllreportData += Process-Permissions -SecurableObject $web -ObjectName $web.Title -ObjectType "Web" -ReportingOnly $true
+Process-Permissions -SecurableObject $web -ObjectName $web.Title -ObjectType "Web" -ReportingOnly $ReportingOnly
 
 # Check permissions for subsites
 foreach ($subWeb in $subWebs) {
-    $AllreportData += Process-Permissions -SecurableObject $subWeb -ObjectName $subWeb.Title -ObjectType "Subsite" -ReportingOnly $true
+    Process-Permissions -SecurableObject $subWeb -ObjectName $subWeb.Title -ObjectType "SubSite" -ReportingOnly $ReportingOnly
 }
 
 # Get all lists and libraries
@@ -178,7 +134,7 @@ $lists = Get-PnPList | Where-Object { $_.Hidden -eq $false -and $excludedLists -
 
 foreach ($list in $lists) {
     # Check permissions for the list/library
-    $AllreportData += Process-Permissions -SecurableObject $list -ObjectName $list.Title -ObjectType "List/Library" -ReportingOnly $true
+    Process-Permissions -SecurableObject $list -ObjectName $list.Title -ObjectType "List/Library" -ReportingOnly $ReportingOnly
     
     # Get folders in the list/library
     $folders = Get-PnPFolder -List $list -Includes ListItemAllFields.HasUniqueRoleAssignments, ListItemAllFields.RoleAssignments
@@ -186,21 +142,22 @@ foreach ($list in $lists) {
     foreach ($folder in $folders) {
         #$folder.ListItemAllFields.HasUniqueRoleAssignments
         #$hasUniquePermissions = Get-PnPProperty -ClientObject $folder -Property HasUniqueRoleAssignments
-        $AllreportData += Process-Permissions -SecurableObject $folder -ObjectName $folder.Name -ObjectType "Folder" -ReportingOnly $true
+        Process-Permissions -SecurableObject $folder -ObjectName $folder.Name -ObjectType "Folder" -ReportingOnly $ReportingOnly
     }
     
     # Get items/files in the list/library
     $items = Get-PnPListItem -List $list -Fields "FileLeafRef"
     foreach ($item in $items) {
         $itemName = $item["FileLeafRef"]
-        $AllreportData += Process-Permissions -SecurableObject $item -ObjectName $itemName -ObjectType "Item/File" -ReportingOnly $true
+        Process-Permissions -SecurableObject $item -ObjectName $itemName -ObjectType "Item/File" -ReportingOnly $ReportingOnly
     }
 }
 
 # Export to Excel if ReportingOnly is true
 if ($ReportingOnly -and $AllreportData.Count -gt 0) {
     $excelPath = "C:\temp\PermissionReport_$(Get-Date -Format 'yyyyMMdd_HHmmss').xlsx"
-    $AllreportData | Export-Excel -Path $excelPath -AutoSize -TableName "PermissionReport" -Title "SharePoint Permission Report"
+    $global:AllReportData  | Export-Excel -Path $excelPath -AutoSize -Title "SharePoint Permission Report" -Show
+    $global:AllReportData  | Out-GridView
     Write-Host "Report exported to $excelPath"
 } elseif ($ReportingOnly -and $reportData.Count -eq 0) {
     Write-Host "No matching permissions found for reporting."
